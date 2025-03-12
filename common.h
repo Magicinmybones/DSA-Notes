ac63d08f0a072eb10cd132286dece0d37a98b0c9
Magicinmybones <150594049+Magicinmybones@users.noreply.github.com>
2025-03-12 11:46:08 +0530

    codes

---
+++ common.h
@@ -0,0 +1,33 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// Standard library headers
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <iomanip>
+#include <fstream>
+#include <functional>
+#include <numeric>
+#include <sstream>
+#include <bitset>
+#include <cassert>
+#include <iterator>
+#include <utility>
+#include <tuple>
+#include <chrono>
+#include <thread>
+#include <climits>
+using namespace std;
+
+// Avoid "using namespace std;" in header files to prevent namespace pollution.
+
+#endif  // COMMON_H
