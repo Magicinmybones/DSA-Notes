1da28bad211fd0591fd0f72e7b727d999a75798f
Magicinmybones <150594049+Magicinmybones@users.noreply.github.com>
2025-03-23 19:12:34 +0530

    Tree Implementation

--- Binary_Tree/Tree_Implementation.cpp
+++ Binary_Tree/Tree_Implementation.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
+#include<queue>
 using namespace std;
 class Node{
     public:
@@ -51,8 +52,45 @@ void PostOrderTraversal_LRN(Node* root){
     PostOrderTraversal_LRN(root->right);
     cout<<root->data<<" ";
 }
+void levelorderTraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+   
+    while(q.size()>1){
+        Node* front = q.front();
+        q.pop(); 
+        if(front == NULL){
+        cout<<endl;
+        q.push(NULL); 
+        }
+        else{
+        cout<<front->data<<" ";
+        if(front->left){
+            q.push(front->left);
+        }
+        if(front->right){
+            q.push(front->right);
+        }
+    }
+    }
+}
 int main(){
     Node* root = createTree();
+    // Example input for a tree with 4 levels:
+    // Enter the following values when prompted:
+    // 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
+    // This will create the following tree:
+    //         1
+    //       /   \
+    //      2     3
+    //     / \   / \
+    //    4   5 6   7
     PreOrderTraversal_NLR(root);
+    cout<<endl;
     InOrderTraversal_LNR(root);
+    cout<<endl;
+    PostOrderTraversal_LRN(root);
+    cout<<endl;
+    levelorderTraversal(root);
 }
